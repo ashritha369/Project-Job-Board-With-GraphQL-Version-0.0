131859d1360d859c52f2fbab4014e700ffe422aa
ashritha369 <ashrithagowda666@gmail.com>
2022-12-09 15:28:45 +0530

    installed the depencies for express and graphql

--- server/server.js
+++ server/server.js
@@ -1,25 +1,29 @@
-import cors from 'cors';
-import express from 'express';
-import { expressjwt } from 'express-jwt';
-import jwt from 'jsonwebtoken';
-import { User } from './db.js';
-
+import cors from "cors";
+import express from "express";
+import { expressjwt } from "express-jwt";
+import jwt from "jsonwebtoken";
+import { User } from "./db.js";
+import { ApolloServer } from "apollo-server-express";
 const PORT = 9000;
-const JWT_SECRET = Buffer.from('Zn8Q5tyZ/G1MHltc4F/gTkVJMlrbKiZt', 'base64');
+const JWT_SECRET = Buffer.from("Zn8Q5tyZ/G1MHltc4F/gTkVJMlrbKiZt", "base64");
 
 const app = express();
-app.use(cors(), express.json(), expressjwt({
-  algorithms: ['HS256'],
-  credentialsRequired: false,
-  secret: JWT_SECRET,
-}));
+app.use(
+  cors(),
+  express.json(),
+  expressjwt({
+    algorithms: ["HS256"],
+    credentialsRequired: false,
+    secret: JWT_SECRET,
+  })
+);
 
-app.post('/login', async (req, res) => {
+app.post("/login", async (req, res) => {
   const { email, password } = req.body;
   const user = await User.findOne((user) => user.email === email);
   if (user && user.password === password) {
     const token = jwt.sign({ sub: user.id }, JWT_SECRET);
-    res.json({ token });  
+    res.json({ token });
   } else {
     res.sendStatus(401);
   }
